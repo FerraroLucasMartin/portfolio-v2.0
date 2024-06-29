b428677132b41bb33f3f64e3f2ea44e21e114c97
lucasFerraro <ferrarolucasmartin@gmail.com>
2024-06-29 00:36:16 -0300

    create tailwind glow class

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -13,6 +13,12 @@ const config: Config = {
         "gradient-conic":
           "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
       },
+      dropShadow: {
+        glow: [
+          "0 0px 10px rgba(255, 255, 255, 0.2)",
+          "0 0px 15px rgba(255, 255, 255, 0.3)",
+        ],
+      },
     },
   },
   plugins: [],
