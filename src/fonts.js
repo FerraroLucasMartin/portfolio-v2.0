f3d4fa2fc7ba4e8447af96dd219a630f8cb2463d
lucasFerraro <ferrarolucasmartin@gmail.com>
2024-06-29 00:36:28 -0300

    modulate fonts importation

---
+++ src/fonts.js
@@ -0,0 +1,5 @@
+import { Inter } from "next/font/google";
+import { Bebas_Neue } from "next/font/google";
+
+export const inter = Inter({ subsets: ["latin"], weight: ["300"] });
+export const bebas = Bebas_Neue({ subsets: ["latin"], weight: ["400"] });
