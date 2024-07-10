b152b84c66a1f0643cbd4664bc8e832d1c2f36c6
lucasFerraro <ferrarolucasmartin@gmail.com>
2024-07-10 16:56:02 -0300

    configs update

--- next.config.mjs
+++ next.config.mjs
@@ -2,6 +2,10 @@
 const nextConfig = {
   output: "export",
   basePath: "/portfolio-v2.0",
+  assetPrefix: "/portfolio-v2.0",
+  images: {
+    unoptimized: true,
+  },
 };
 
 export default nextConfig;
